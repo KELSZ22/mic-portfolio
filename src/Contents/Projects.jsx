4126fb08ef451e39055f2e71d15d1cf3d90edb9d
KELSZ22 <kelgithub@gmail.com>
2025-01-03 12:49:47 +0800

    projects updated

--- src/Contents/Projects.jsx
+++ src/Contents/Projects.jsx
@@ -24,12 +24,12 @@ function Project() {
 
                       <div className="proj-card">
 
-            <img src="/webbased.png" alt="webbased.png" />
+            <img src="/wordpress.png" alt="wordpress.png" />
          
       
            </div>
            <div className="proj-content">
-              <h1>Web-based Attendance Management System using QR Code</h1>
+              <h1>Wordpress Portfolio</h1>
               <h3>Date</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
               
@@ -37,12 +37,12 @@ function Project() {
 
                       <div className="proj-card">
 
-            <img src="/webbased.png" alt="webbased.png" />
+            <img src="/salesbilling.png" alt="salesbilling.png" />
          
       
            </div>
            <div className="proj-content">
-              <h1>Web-based Attendance Management System using QR Code</h1>
+              <h1>Sales and Billing System</h1>
               <h3>Date</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
               
@@ -50,12 +50,12 @@ function Project() {
 
                       <div className="proj-card">
 
-            <img src="/webbased.png" alt="webbased.png" />
+            <img src="/batteryApp.png" alt="batteryApp.png" />
          
       
            </div>
            <div className="proj-content">
-              <h1>Web-based Attendance Management System using QR Code</h1>
+              <h1>Battery Life Safer App: Integrated with ESP32 arduino</h1>
               <h3>Date</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
               
@@ -63,12 +63,13 @@ function Project() {
 
                       <div className="proj-card">
 
-            <img src="/webbased.png" alt="webbased.png" />
+            <img src="/inventory.png" alt="inventory.png" />
          
       
            </div>
+
            <div className="proj-content">
-              <h1>Web-based Attendance Management System using QR Code</h1>
+              <h1>Inventory Management System</h1>
               <h3>Date</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
               
@@ -76,16 +77,31 @@ function Project() {
 
                       <div className="proj-card">
 
-            <img src="/webbased.png" alt="webbased.png" />
+            <img src="/udm.png" alt="udm.png" />
          
       
            </div>
            <div className="proj-content">
-              <h1>Web-based Attendance Management System using QR Code</h1>
+              <h1>Library Management System</h1>
               <h3>Date</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
               
               </div>
+
+  
+                      <div className="proj-card">
+
+              <iframe src="https://mi-calculator-eight.vercel.app/"  width="350" height="350px"  allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" title="Address"></iframe>         
+      
+           </div>
+           <div className="proj-content">
+              <h1>MiCalculator : Ver.1.0</h1>
+              <h3>Date</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              
+              </div>
+
+              
       </div>
 
 
