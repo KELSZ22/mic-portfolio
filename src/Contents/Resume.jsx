e4798d73a255202556ba0ce05a67219a558c1be2
KELSZ22 <kelgithub@gmail.com>
2025-03-26 15:58:05 +0800

    resume updates

--- src/Contents/Resume.jsx
+++ src/Contents/Resume.jsx
@@ -1,115 +1,174 @@
-
 function Resume() {
-  
-    
-    return(
-            <>
-        <h1 className="resume-header"> Resume</h1>
+  return (
+    <>
+      <h1 className="resume-header"> Resume</h1>
 
-        <div className="resume-container">
+      <div className="resume-container">
+        <div className="first-column">
+          <img src="/michaelpogi.png" alt="michaelpogi.png" height="200px" />
+          <br />
+          <label htmlFor="">Michael Z. Sabino</label>
 
+          <div className="personal-details">
+            <hr />
+            <label htmlFor="">Personal Details</label>
+            <p>
+              <b>Date of Birth:</b> October 24,2001
+            </p>
+            <p>
+              <b>Contact:</b> 0935-869-3624
+            </p>
+            <p>
+              <b>Address:</b> 1319 - A Abad Santos St. Ext. Moriones Tondo,
+              Manila
+            </p>
+            <p>
+              <b>Email:</b> michaels6355@gmail,com
+            </p>
 
-            <div className="first-column">
-                <img src="/michaelpogi.png" alt="michaelpogi.png" height="200px"/>
-                <br />
-                <label htmlFor="">Michael Z. Sabino</label>
-                
-                <div className="personal-details">
-                <hr />
-                    <label htmlFor="">Personal Details</label>
-                    <p><b>Date of Birth:</b> October 24,2001</p>
-                    <p><b>Contact:</b> 0935-869-3624</p>
-                    <p><b>Address:</b> 1319 - A Abad Santos St. Ext. Moriones Tondo, Manila</p>
-                    <p><b>Email:</b> michaels6355@gmail,com</p>
+            <hr />
 
-                    <hr />
+            <label htmlFor=""> Achievements</label>
+            <p>Tesda Java Programming (NC III) batch 3: Top 1</p>
+            <p>1st year Dean's Lister</p>
+            <p>Senior High School with Honors</p>
 
-                    <label htmlFor=""> Achievements</label>
-                    <p>Tesda Java Programming (NC III) batch 3: Top 1</p>
-                    <p>1st year Dean's Lister</p>
-                    <p>Senior High School with Honors</p>
+            <hr />
 
-                    <hr />
+            <label htmlFor=""> Languages</label>
+            <p>Tagalog</p>
+            <p>English</p>
 
-                    <label htmlFor=""> Languages</label>
-                    <p>Tagalog</p>
-                    <p>English</p>
+            <hr />
+            <label htmlFor="">Character References</label>
+            <p>
+              <b>Name: </b> Lhorelie Arcega
+            </p>
+            <p>
+              <b>Contact: </b>0930-014-4082
+            </p>
 
-                    <hr />
-                    <label htmlFor="">Character References</label>
-                    <p><b>Name: </b> Lhorelie Arcega</p>
-                    <p><b>Contact: </b>0930-014-4082</p>
-
-                    <p><b>Name: </b> Engr. Gideon Navarro</p>
-                    <p><b>Contact: </b>0926-750-8383</p>
-
-                </div>
-            </div>
-            
-            <div className="second-column">
-                <label htmlFor="">Career Objective</label>
-                <p>I am seeking a chance to apply my skills and capabilities to contribute positively to an organization. With a quick learning ability, I can thrive in a fast-paced work environment, demonstrating effective performance. I am dedicated to ongoing learning and eager to embrace new challenges.</p>
-                
-                <hr />
-
-                <label htmlFor="">Experience </label>
-                 <br />
-                <label htmlFor="">On the Job Training</label>
-                <p>Intellyx ICT, 2023</p>
-                <p>Negosyante I.T Solutions Corp., 2003</p>
-                <p>Work Immersion: First Academy of Computer Arts, 2020</p>
-
-                <hr />
-
-                <label htmlFor="">Education</label>
-                
-                <p><b>Bachelor of Science in Computer Engineering</b></p>
-                <p>Universidad De Manila</p>
-
-                <p><b>Information Communication Technology (ICT)</b></p>
-                <p>Arellano University</p>
-
-                <p><b>High School</b></p>
-                <p>Tondo High School</p>
-
-                <p><b>Elementary</b></p>
-                <p>Manuel L. Quezon Elementary School</p>
-
-                <hr />
-
-                <label htmlFor="">Skills</label>
-                <ul>
-                    <li>Programming languages (Java,Python, PHP and C++)</li>
-                    <li>Web Development (HTML, CSS, Javascript, Laravel
-                        Framework, Cordova, WordPress, Node js and React js)</li>
-                    <li>Database (SQL, MariaDB, MongoDB and SQlite)</li>
-
-                    <li>Editing (Adobe Photoshop, Adobe Illustrator,Autodesk and Figma)</li>
-                    <li>Time Management</li>
-                    <li>Adaptability</li>
-                </ul>
-
-                <hr />
-                <label htmlFor="">Certificates</label>
-                <p><a href="https://drive.google.com/file/d/17FtjcdJMLRzvIXOoW84tvYfH_sBIZV3s/view" target="_blank">TESDA NC III Java Programming (2023)</a></p>
-                <p><a href="https://drive.google.com/file/d/1PPe6mGbWgYFXYGVHMySRevQ4ajcX_jVI/view" target="_blank">TESDA Developing Designs for User Interface (2024)</a> 
-                </p>
-                <p><a href="https://drive.google.com/file/d/10mHEXIuHHn4_pHAzfVwFGEfEocv6XNf7/view" target="_blank">TESDA Developing Designs for User Experience (2024)</a></p>
-                
-                <p><a href="https://drive.google.com/file/d/16CozmRDm6Uv8J9VgvhtmumSEhdxu3IcQ/view" target="_blank">Frontend Fundamentals Bootcamp (2024)</a></p>
-                <p><a href="https://drive.google.com/file/d/1j_v82_tgKkJNLjFA-4uVMk1M6I4sh_Gn/view" target="_blank">Data Analytics Fundamentals Bootcamp (2024)</a></p>
-
-
-  
-            </div>
-            
+            <p>
+              <b>Name: </b> Engr. Gideon Navarro
+            </p>
+            <p>
+              <b>Contact: </b>0926-750-8383
+            </p>
+          </div>
         </div>
-            
-            
-            </>
 
-    );
-    
-    
+        <div className="second-column">
+          <label htmlFor="">Career Objective</label>
+          <p>
+            I am seeking a chance to apply my skills and capabilities to
+            contribute positively to an organization. With a quick learning
+            ability, I can thrive in a fast-paced work environment,
+            demonstrating effective performance. I am dedicated to ongoing
+            learning and eager to embrace new challenges.
+          </p>
+
+          <hr />
+
+          <label htmlFor="">Experience </label>
+          <br />
+          <p>Spring Board Ph., 2025</p>
+          <p>Web Developer</p>
+          <br />
+
+          <label htmlFor="">On the Job Training</label>
+          <p>Intellyx ICT, 2023</p>
+          <p>Negosyante I.T Solutions Corp., 2003</p>
+          <p>Web Developer</p>
+          <p>Work Immersion: First Academy of Computer Arts, 2020</p>
+
+          <hr />
+
+          <label htmlFor="">Education</label>
+
+          <p>
+            <b>Bachelor of Science in Computer Engineering</b>
+          </p>
+          <p>Universidad De Manila</p>
+
+          <p>
+            <b>Information Communication Technology (ICT)</b>
+          </p>
+          <p>Arellano University</p>
+
+          <p>
+            <b>High School</b>
+          </p>
+          <p>Tondo High School</p>
+
+          <p>
+            <b>Elementary</b>
+          </p>
+          <p>Manuel L. Quezon Elementary School</p>
+
+          <hr />
+
+          <label htmlFor="">Skills</label>
+          <ul>
+            <li>Programming languages (Java,Python, PHP and C++)</li>
+            <li>
+              Web Development (HTML, CSS, Javascript, Laravel Framework,
+              Cordova, WordPress, Node js and React js)
+            </li>
+            <li>Database (SQL, MariaDB, MongoDB and SQlite)</li>
+
+            <li>
+              Editing (Adobe Photoshop, Adobe Illustrator,Autodesk and Figma)
+            </li>
+            <li>Time Management</li>
+            <li>Adaptability</li>
+          </ul>
+
+          <hr />
+          <label htmlFor="">Certificates</label>
+          <p>
+            <a
+              href="https://drive.google.com/file/d/17FtjcdJMLRzvIXOoW84tvYfH_sBIZV3s/view"
+              target="_blank"
+            >
+              TESDA NC III Java Programming (2023)
+            </a>
+          </p>
+          <p>
+            <a
+              href="https://drive.google.com/file/d/1PPe6mGbWgYFXYGVHMySRevQ4ajcX_jVI/view"
+              target="_blank"
+            >
+              TESDA Developing Designs for User Interface (2024)
+            </a>
+          </p>
+          <p>
+            <a
+              href="https://drive.google.com/file/d/10mHEXIuHHn4_pHAzfVwFGEfEocv6XNf7/view"
+              target="_blank"
+            >
+              TESDA Developing Designs for User Experience (2024)
+            </a>
+          </p>
+
+          <p>
+            <a
+              href="https://drive.google.com/file/d/16CozmRDm6Uv8J9VgvhtmumSEhdxu3IcQ/view"
+              target="_blank"
+            >
+              Frontend Fundamentals Bootcamp (2024)
+            </a>
+          </p>
+          <p>
+            <a
+              href="https://drive.google.com/file/d/1j_v82_tgKkJNLjFA-4uVMk1M6I4sh_Gn/view"
+              target="_blank"
+            >
+              Data Analytics Fundamentals Bootcamp (2024)
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
 export default Resume;
